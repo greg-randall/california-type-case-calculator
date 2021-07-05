f69fa188147a8c33cfaf3244c1bf8ffcd4ffc3eb
Greg Randall <grandall@sheltonstate.edu>
2021-07-04 22:34:07 -0500

    added several cleaning/verification steps, fixed a bug with the period

--- process.php
+++ process.php
@@ -20,25 +20,65 @@
 <p>
 <?php
 
-$page = str_split($_GET['page']);
-
-foreach ($page as $char)
-{
-    if (array_key_exists($char, $_GET))
-    {
-        if ($_GET[$char] >= 1)
-        {
-            echo "<span style=\"color:green;\">$char</span>";
-            $_GET[$char]--;
-        }else{echo "<span style=\"color:red;\">$char</span>";}
-    }else
-    {
-        echo "<span style=\"color:orange;\">$char</span>";
-    }
+//collects the list of valid letters
+foreach (explode("\n", file_get_contents("valid_letters.txt")) as $letter) {
+    $valid_letters[trim($letter)] = "";
 }
 
+//sanatize
+foreach ($_POST as $key => $value) {
+    if ($key == "period") { //seems like php won't take a "." as post/get input??
+        $key = ".";
+    }
+    
+    //makes sure the recieved variable is a valid letter and isn't our page of text
+    if ($key != "page" && array_key_exists($key, $valid_letters)) {
+        //$key = urldecode($key);
+        $value = trim($value); //make sure all the counts of letters are integer numbers
+        if (is_numeric($value)) {
+            $value           = floor($value);
+            $sanitized[$key] = $value;
+            
+        } else {
+            $sanitized[$key] = 0;
+        }
+    } elseif ($key == "page") { //strip html from the page input
+        $value           = strip_tags($value);
+        $sanitized[$key] = $value;
+    }
+    $missing[$key] = 0;
+    
+}
 
-?></p>
+//parse string and count if there are enough letters
+$page = str_split($sanitized['page']);
+foreach ($page as $char) {
+    if ($char != " ") {
+        if (array_key_exists($char, $sanitized)) {
+            if ($sanitized[$char] >= 1) {
+                echo "<span style=\"background-color:#b8ffdc;\">$char</span>"; //good
+                $sanitized[$char]--;
+            } else {
+                echo "<span style=\"background-color:#ffb8b8;\">$char</span>"; //bad
+                $missing[$char]++;
+            }
+        } else {
+            echo "<span style=\"background-color:#ffe6b8;\">$char</span>"; //unkown
+        }
+    } else {
+        echo $char;
+    }
+}
+echo "<Br>";
+
+foreach ($missing as $key => $value) {
+    if ($value > 0) {
+        echo "Need $value more $key<br>";
+    }
+}
+?>
+
+</p>
 </div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">
 	</script>
