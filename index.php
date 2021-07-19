86762cda68723d28d9aeb7c28fd45d48f7520629
Greg Randall <grandall@sheltonstate.edu>
2021-07-19 18:34:06 -0500

    three modes of input added, direct, weight, and previous. bugs fixed, and new bugs

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport"><!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<title></title>
+	<style>
+
+
+	       .input-size {
+	           width:30px;
+	       }
+	</style>
+</head>
+<body>
+	<div class="container">
+		<form action="process.php" method="post" style="margin-bottom:25px;">
+			<h2>Page of Text</h2>
+			<textarea class="form-control" name="page" rows="10" style="min-width: 100%">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. `1234567890-= ~!@#$%^*()_+ ,;[/']\ &lt;:{?}|</textarea><br>
+			<br>
+			
+			<input id="weight" name="weight" onclick="javascript:yesnoCheck();" type="radio"> Enter the weight of a single piece of type, and then the total weight of all that piece<br>
+			<input id="direct" name="direct" onclick="javascript:yesnoCheck();" type="radio"> Enter the count of each piece<br>
+			<input id="previous" name="previous" onclick="javascript:yesnoCheck();" type="radio"> Paste in previously generated type file<br>
+			<br>
+			<div id="ifweight" style="display:none">
+				<h2>Weight of One</h2>
+				<?php echo str_ireplace("name=\"", "name=\"weight_one_", file_get_contents('base-cali-case.html')); ?><br>
+				<br>
+				<h2>Weight of All</h2>
+				<?php echo str_ireplace("name=\"", "name=\"weight_total_", file_get_contents('base-cali-case.html')); ?><br>
+			</div>
+			<div id="ifdirect" style="display:none">
+			    <h2>Count of Each Glyph</h2>
+				<?php echo file_get_contents('base-cali-case.html'); ?>
+			</div>
+			<div id="ifprevious" style="display:none">
+				<h2>Previously Used Typecase</h2>
+				<textarea class="form-control" name="previous_data" style="min-width: 100%"></textarea><br>
+				<br>
+			</div><br>
+			<input class="text-center" type="submit">
+		</form>
+	</div>
+	<script type="text/javascript">
+
+	function yesnoCheck() {
+	   if (document.getElementById('weight').checked) {
+	       document.getElementById('ifweight').style.display = 'block';
+	   }else{
+	       document.getElementById('ifweight').style.display = 'none';
+	   }
+
+	   if (document.getElementById('direct').checked) {
+	       document.getElementById('ifdirect').style.display = 'block';
+	   }else{
+	       document.getElementById('ifdirect').style.display = 'none';
+	   }
+
+	   if (document.getElementById('previous').checked) {
+	       document.getElementById('ifprevious').style.display = 'block';
+	   }else{
+	       document.getElementById('ifprevious').style.display = 'none';
+	   }
+
+
+	}
+   
+    // in case someone hits the back button the radio button that is selected shows
+    //const body = document.body;
+    //body.onload = yesnoCheck;
+
+
+
+	</script> 
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">
+	</script>
+</body>
+</html>
--- process.php
+++ process.php
@@ -1,88 +1,166 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta content="width=device-width, initial-scale=1" name="viewport"><!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-	<title></title>
-	<style>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport"><!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title></title>
+    <style>
 
 
-		.input-size {
-			width:20px;
-		}
-	</style>
+        .input-size {
+            width:20px;
+        }
+    </style>
 </head>
 <body><br><br>
-	<div class="container">
+    <div class="container" style="margin-bottom:30px;">
 
 <p>
 <?php
+//echo "<pre>".print_r($_POST,true)."</pre>";
+$valid_letters = array( 'ffi', 'fl', '/', '‘', '’', 'k', 'e', 'j', 'b', 'c', 'd', '?', '!', 'l', 'm', 'n', 'h', 'z', 'x', 'v', 'u', 't', '4_to_em', 'q', '1', '2', '3', '4', '5', '6', '7', '8', 'i', 's', 'f', 'g', 'ff', '9', 'fi', '0', 'o', 'y', 'p', 'w', ',', '3_to_em', 'en_quad', 'a', 'r', ';', ':', 'em_quads', '.', '-', '$', '–', '—', '(', ')', '[', ']', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z', 'J', 'U', '&', 'ffl');
 
-//collects the list of valid letters
-foreach (explode("\n", file_get_contents("valid_letters.txt")) as $letter) {
-    $valid_letters[trim($letter)] = "";
-}
+//collects the list of valid letters & sanatizes 
+/*
+====================================
+deal with zero and period and left/right square brackets
 
-//sanatize
-foreach ($_POST as $key => $value) {
-    if ($key == "period") { //seems like php won't take a "." as post/get input??
-        $key = ".";
-    }
-    
-    //makes sure the recieved variable is a valid letter and isn't our page of text
-    if ($key != "page" && array_key_exists($key, $valid_letters)) {
-        //$key = urldecode($key);
-        $value = trim($value); //make sure all the counts of letters are integer numbers
-        if (is_numeric($value)) {
-            $value           = floor($value);
-            $sanitized[$key] = $value;
-            
-        } else {
-            $sanitized[$key] = 0;
-        }
-    } elseif ($key == "page") { //strip html from the page input
-        $value           = strip_tags($value);
-        $sanitized[$key] = $value;
-    }
-    $missing[$key] = 0;
-    
-}
+====================================
+*/
 
-//parse string and count if there are enough letters
-$page = str_split($sanitized['page']);
-foreach ($page as $char) {
-    if ($char != " ") {
-        if (array_key_exists($char, $sanitized)) {
-            if ($sanitized[$char] >= 1) {
-                echo "<span style=\"background-color:#b8ffdc;\">$char</span>"; //good
-                $sanitized[$char]--;
+
+
+if (key_exists("weight", $_POST)) {
+    foreach ($valid_letters as $letter) {
+        //make sure both keys exist
+        if (key_exists("weight_one_$letter", $_POST) && key_exists("weight_total_$letter", $_POST)) {
+            //make sure both weights aren't null and are numbers
+            if (is_numeric($_POST["weight_one_$letter"]) && is_numeric($_POST["weight_total_$letter"]) ) {
+                //echo "$letter -- " . $_POST["weight_total_$letter"] ."/". $_POST["weight_one_$letter"] ."<br>";
+                $computed = floor($_POST["weight_total_$letter"] / $_POST["weight_one_$letter"]);
+                //make sure the number makes sense, if the number is one or greater accept it otherwise set to zero
+                if ($computed >= 1) {
+                    $letters_calc[$letter] = $computed;
+                } else {
+                    $letters_calc[$letter] = 0;
+                }
             } else {
-                echo "<span style=\"background-color:#ffb8b8;\">$char</span>"; //bad
-                $missing[$char]++;
+                $letters_calc[$letter] = 0; //if keys are null or non-numeric set to zero
             }
         } else {
-            echo "<span style=\"background-color:#ffe6b8;\">$char</span>"; //unkown
+            $letters_calc[$letter] = 0; //if both keys don't exist set the letter to zero
+        }
+    }
+} else if (key_exists("previous", $_POST)) {
+    $peviously_computed = json_decode($_POST['previous_data'], true);
+    foreach ($valid_letters as $letter) {
+        if (key_exists($letter, $peviously_computed)) {
+            if ($peviously_computed[$letter] < 1) {
+                $letters_calc[$letter] = 0;
+            } else {
+                $letters_calc[$letter] = $peviously_computed[$letter];
+            }
+        }
+    }
+    //echo "<pre>".print_r($peviously_computed,true)."</pre>";
+} else if (key_exists("direct", $_POST)) {
+    foreach ($valid_letters as $letter) {
+        if (key_exists("$letter", $_POST)) {
+            if (!is_null($_POST["$letter"]) && is_numeric($_POST["$letter"])) {
+                if ($_POST["$letter"] < 1) {
+                    $letters_calc[$letter] = 0;
+                } else {
+                    $letters_calc[$letter] = $_POST["$letter"];
+                }
+            } else {
+                $letters_calc[$letter] = 0;
+            }
+        } else {
+            $letters_calc[$letter] = 0;
+        }
+    }
+} else {
+    echo "<p>Please select a radio button on the previous screen.</p>";
+    exit();
+}
+$page = strip_tags($_POST["page"]);
+
+$for_next_time = json_encode($letters_calc);
+
+
+//parse string and count if there are enough letters
+$unknown = array();
+$missing = array();
+
+$enough_of_everything = true;
+$page                 = str_split($page);
+foreach ($page as $char) {
+    if ($char != " ") {
+        if (array_key_exists($char, $letters_calc)) {
+            if ($letters_calc[$char] >= 1) {
+                echo "<span style=\"background-color:#b8ffdc;\">$char</span>"; //good
+                $letters_calc[$char]--;
+            } else {
+                echo "<span style=\"background-color:#ffb8b8;\">$char</span>"; //bad
+                if (!array_key_exists($char, $missing)) {
+                    $missing[$char] = 0;
+                }
+                $missing[$char]++;
+                $enough_of_everything = false;
+            }
+        } else {
+            echo "<span style=\"background-color:#ffe6b8;\">$char</span>"; //unknown
+            if (!array_key_exists($char, $unknown)) {
+                $unknown[$char] = 0;
+            }
+            $unknown[$char]++;
         }
     } else {
         echo $char;
     }
 }
-echo "</p><Br><ul>";
+echo "</p>
 
+<ul style=\"width: 70%;margin: auto\"><li style=\"margin-right:30px; display: inline;\"><span style=\"background-color:#b8ffdc;\">Enough Letters</span></li><li style=\"margin-right:30px; display: inline;\"><span style=\"background-color:#ffb8b8;\">Not Enough Letters</span></li><li style=\"margin-right:30px; display: inline;\"><span style=\"background-color:#ffe6b8;\">Unknown Letters</span></li></ul>
+
+<hr>";
+
+if ($enough_of_everything) {
+    echo "<h3 class=\"text-center\">You have enough letters to print the page that you entered!</h3>";
+} else {
+    echo "<h3 class=\"text-center\">You do not have enough letters to print the page that you entered.</h3>";
+}
+
+echo "<br><br><div class=\"row\"><div class=\"col-md-6\"><ul>";
 arsort($missing);
 
 foreach ($missing as $key => $value) {
     if ($value > 0) {
-        echo "Need $value more $key<br>";
+        echo "<li>Need $value more of $key.</li>";
     }
 }
+
+echo "</ul></div><div class=\"col-md-6\"><ul>";
+arsort($unknown);
+
+foreach ($unknown as $key => $value) {
+    if ($value > 0) {
+        echo "<li>Not sure what $key is, but there were $value.</li>";
+    }
+}
+
 ?></ul>
 
-
+</div></div>
+<hr>
+<h2>Save this output for use next time:</h2>
+<div style="width:80%;"><form><textarea class="form-control" rows="5" style="min-width: 100%">
+<?php echo $for_next_time; ?>
+</textarea></form></div>
 </div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">
-	</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js">
+    </script>
 </body>
 </html>
